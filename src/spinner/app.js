2fcb6a40cd92b79c65b03c8fa26dcd984e6a6c38
Ilan Copelyn <ilan@netreturn.co.za>
2017-06-21 23:36:31 +0200

    spinner

---
+++ src/spinner/app.js
@@ -0,0 +1,13 @@
+console.log('app.js');
+// Undo "prevent flash of unstyled content" - see <body style="display:none;"> in index.html
+// NOTE: if there are .js errors then <body> is not displayed - undo below
+//document.getElementsByTagName('body')[0].style.display = "";
+
+// deps
+import riot from 'riot'
+import $ from 'jquery'
+import '../all/site.css'
+import './app.tag.html'
+
+riot.mount('app')
+$('body').show();
---
+++ src/spinner/webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	target: 'web',
+	devtool: 'inline-source-map',
+
+	resolve: {
+		extensions: [
+			".js"
+		]
+	    },
+	entry: {
+		app: [
+			"./src/spinner/app.js"
+		]
+	},
+	output: {
+		path: path.join(__dirname, '/../../dist/spinner/'),
+		filename: "[name].min.js"
+	},
+	module: {
+		rules: [
+      {
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader', options: { base: 2000 } },
+					'css-loader'
+				]
+			},
+			{
+				test: /\.tag(.html)?$/,
+        exclude: /node_modules/,
+        loader: 'riot-tag-loader',
+        options: {
+          //type: 'es6', // NO!!!
+          hot: true,
+          debug: true
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: 'body',
+			hash: true,
+			template: './src/spinner/index.html',
+			filename: 'index.html'
+		}),
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery"	// for UploaderJS
+		})
+	]
+};
