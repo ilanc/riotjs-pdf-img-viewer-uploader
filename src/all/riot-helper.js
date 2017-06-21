a8ecc132bfbaa6444578afb2b602eb05ca9fc2d1
Ilan Copelyn <ilan@netreturn.co.za>
2017-06-22 01:43:24 +0200

    app - download button = popup & start spinner

---
+++ src/all/riot-helper.js
@@ -0,0 +1,16 @@
+// https://github.com/riot/riot/issues/934
+//import $ from 'jquery'
+
+export function domToTag(el) {
+  return el._tag;
+}
+
+export function jqueryToTag(selector, startNode) {
+  //var el = $(selector, startNode); // jquery doesn't work when called from 'mount' but does from console whilst single stepping?
+  if (!startNode.root)
+    return undefined;
+  var el = startNode.root.querySelectorAll(selector);
+  if (!el || !el.length)
+    return undefined;
+  return el[0]._tag;
+}
