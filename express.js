1a60b906c725568d631dcbd2a798080398de0f48
Ilan Copelyn <ilan@netreturn.co.za>
2017-06-22 21:13:46 +0200

    express.js - download pdf

--- express.js
+++ express.js
@@ -3,118 +3,152 @@ import path from 'path';
 import fs from 'fs';
 import open from 'open';
 import webpack from 'webpack';
-import config from './ux/capture/webpack.app.config';
 //import compression from 'compression';
 
-const port = 3000;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Config
+
+const _PORT = 3000;
+const _DIST = "dist/";
+const _SRC = "src/";
+const _MODULE = "pdf-img-viewer-uploader";
+const _FLOW_JSON = 'express.json';
+
+var config = require('./src/'+_MODULE+'/webpack.config');
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 const app = express();
 const compiler = webpack(config);
-app.use(express.static(path.join(__dirname, 'wwwroot/')));
+app.use(express.static(path.join(__dirname, _DIST)));
 //app.use(compression());
 
 app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  //publicPath: config.output.publicPath
-  publicPath: '/capture'
-  //publicPath: '/'
+ //noInfo: true,
+ publicPath: '/pdf-img-viewer-uploader/'
 }));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'ux/root.html'));
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// URL = /
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, _SRC + 'root.html'));
 });
 
-app.get('/capture/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'wwwroot/capture/index.html'));
+// URL = /_MODULE/
+// NOTE: allows ../ relative paths work from dist/module/index.html
+app.get('/' + _MODULE + '/', function (req, res) {
+  res.sendFile(path.join(__dirname, _DIST + _MODULE + '/index.html'));
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Flow API
+
+// .json specifies:
+//  'url': response setting
+//
+// response setting = response config, or [ response configs ]
+//  setting can be a single response config or an array of response configs
+//  in the case of an array, iterate through response configs every time url is called
+/*
+      single response config:
+        "/module/api1": "api/module/api1/responses/success.json",                             // string
+        "/module/api1/": { response: "api/module/api1/responses/success.json", delay: 1000 }  // object
+
+      array of response configs:
+        "/module/api2/": [
+          "api/module/api2/responses/wrongpin.json",                                          // string
+          { response: "api/module/api2/responses/success.json", delay: 1000 }                 // object
+        ]
+*/
+//
+// response config:
+//  can be a string (file = response .json)
+//  or object (file & delay)
 
 var flow;
 function ReadFlowApi() {
-	var json = fs.readFileSync('express.json', 'utf8');
-	json = json.replace(/\/\*[\s\S]*?\*\//gm, ''); // strip /* .. */ comments
-	flow = JSON.parse(json);
+  var json = fs.readFileSync(_FLOW_JSON, 'utf8');
+  json = json.replace(/\/\*[\s\S]*?\*\//gm, ''); // strip /* .. */ comments
+  flow = JSON.parse(json);
 }
 ReadFlowApi();
 
 function FlowApi(req, res) {
-	var q = Object.keys(req.query)[0]; // url?1
-	var f = req.query.f ? req.query.f : q ? q : '1'; // url?f=1 || url?1 || 1
-	if (typeof x === 'undefined') f = '1';
-	var url = req.path;
-	url = url.replace(/\/?$/, '/'); // trailling slash
-	url = url.toLowerCase();
-	f = f.toLowerCase();
-	if (flow[f] && flow[f][url]) {
-		// single config or array of configs?
-		/*
-		// single config
-			// string
-			"/capture/sendemail/": "ux/capture/api/responses/capture/sendemail/success.json",
-			// object
-			"/capture/sendemail/": { response: "ux/capture/api/responses/capture/sendemail/success.json", delay: 1000 }
+  var q = Object.keys(req.query)[0]; // url?1
+  var f = req.query.f ? req.query.f : q ? q : '1'; // url?f=1 || url?1 || 1
+  if (typeof x === 'undefined') f = '1';
+  var url = req.path;
+  url = url.replace(/\/?$/, '/'); // trailling slash
+  url = url.toLowerCase();
+  f = f.toLowerCase();
+  if (flow[f] && flow[f][url]) {
 
-		// array of configs
-			"/capture/emailack/": [
-				"ux/capture/api/responses/capture/emailack/wrongpin.json", // string
-				{ response: "ux/capture/api/responses/capture/sendemail/success.json", delay: 1000 } // object
-			]
-		*/
-		let config = flow[f][url];
+    // 1. setting can be a single config or an array of configs
+    let setting = flow[f][url];
+    let config;
+    if (Array.isArray(setting)) {
+      // array of configs
+      let arr = setting;
+      config = FlowApiStep(f, url, arr); // iterate through configs every time url is called
+    } else {
+      // single config
+      config = setting;
+    }
 
-		// array of configs
-		if (Array.isArray(config)) {
-				let arr = config;
-				config = FlowApiStep(f, url, arr);
-		}
+    // 2. Process response config - string || object
+    let responseJson;
+    let delay = 0;
+    if (typeof config === 'string') {
+      responseJson = config;
+    } else {
+      responseJson = config.response;
+      delay = config.delay;
+    }
 
-		// process single config
-		let responseJson;
-		let delay = 0;
-		if (typeof config === 'string') {
-			responseJson = config;
-		} else {
-			responseJson = config.response;
-			delay = config.delay;
-		}
-
-		let file = path.join(__dirname, responseJson);
-		if (delay)  {
-			setTimeout(function() {
-				FlowApiResponse(res, file)
-			}, delay);
-		} else {
-			FlowApiResponse(res, file)
-		}
-	} else {
-		res.sendStatus(404);
-	}
+    // do response
+    let file = path.join(__dirname, responseJson);
+    if (delay) {
+      setTimeout(function () {
+        FlowApiResponse(res, file)
+      }, delay);
+    } else {
+      FlowApiResponse(res, file)
+    }
+  } else {
+    res.sendStatus(404);
+  }
 }
 
 function FlowApiResponse(res, file) {
-	if (fs.existsSync(file)) {
-		res.sendFile(file);
-	} else {
-		res.sendStatus(404);
-	}
+  if (fs.existsSync(file)) {
+    res.sendFile(file);
+  } else {
+    res.sendStatus(404);
+  }
 }
 
 function FlowApiStep(f, url, arr) {
-	var x = flow[f]['#'+url];
-	let count = (typeof x === 'undefined') ? -1 : x;
-	++count;
-	count = count % arr.length;
-	flow[f]['#'+url] = count;
-	return arr[count];
-	//for (var i = 0; i < arr.length; ++i) {
-	//let config = arr[i];
-	//}
+  var x = flow[f]['#' + url];
+  let count = (typeof x === 'undefined') ? -1 : x;
+  ++count;
+  count = count % arr.length;
+  flow[f]['#' + url] = count;
+  return arr[count];
+  //for (var i = 0; i < arr.length; ++i) {
+  //let config = arr[i];
+  //}
 }
 
 app.all('/*', FlowApi);
 
-app.listen(port, function(err) {
+// end Flow API
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.listen(_PORT, function (err) {
   if (err) {
     console.log(err);
   } else {
-    open('http://localhost:' + port);
+    open('http://localhost:' + _PORT);
   }
 });
--- src/pdf-img-viewer-uploader/webpack.config.js
+++ src/pdf-img-viewer-uploader/webpack.config.js
@@ -1,48 +1,49 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	target: 'web',
-	devtool: 'inline-source-map',
+  target: 'web',
+  devtool: 'inline-source-map',
 
-	resolve: {
-		extensions: [
-			".js"
-		]
-	    },
-	entry: {
-		app: [
-			"./src/pdf-img-viewer-uploader/app.js"
-		]
-	},
-	output: {
-		path: path.join(__dirname, '/../../dist/pdf-img-viewer-uploader/'),
-		filename: "[name].min.js"
-	},
-	module: {
-		rules: [
+  resolve: {
+    extensions: [
+      '.js'
+    ]
+  },
+  entry: {
+    app: [
+      './src/pdf-img-viewer-uploader/app.js'
+    ]
+  },
+  output: {
+    path: path.join(__dirname, '/../../dist/pdf-img-viewer-uploader/'),
+    //publicPath: '/pdf-img-viewer-uploader/',
+    filename: '[name].min.js'
+  },
+  module: {
+    rules: [
       {
-				test: /\.css$/,
-				use: [
-					{ loader: 'style-loader', options: { base: 2000 } },
-					'css-loader'
-				]
-			},
-			{
-				test: /\.(eot|svg|ttf|woff|woff2)/,
-				include: /fontawesome-webfont/, // exclude app svgs (e.g. ../all/app/icon-camera.svg from ./app.css)
-				loader: 'file-loader',
-				options: { name: 'fonts/[name].[ext]' }
-			},
-			{
-				test: /\.(png|jpg|jpeg|gif|svg)$/,
-				exclude: /fontawesome-webfont/, // fontawesome-webfont.svg
-				loader: 'file-loader',
-				options: { name: 'img/[name].[ext]' }
-			},
-			{
-				test: /\.tag(.html)?$/,
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader', options: { base: 2000 } },
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)/,
+        include: /fontawesome-webfont/, // exclude app svgs (e.g. ../all/app/icon-camera.svg from ./app.css)
+        loader: 'file-loader',
+        options: { name: 'fonts/[name].[ext]' }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        exclude: /fontawesome-webfont/, // fontawesome-webfont.svg
+        loader: 'file-loader',
+        options: { name: 'img/[name].[ext]' }
+      },
+      {
+        test: /\.tag(.html)?$/,
         exclude: /node_modules/,
         loader: 'riot-tag-loader',
         options: {
@@ -56,18 +57,18 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       }
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			inject: 'body',
-			hash: true,
-			template: './src/pdf-img-viewer-uploader/index.html',
-			filename: 'index.html'
-		}),
-		new webpack.ProvidePlugin({
-			$: "jquery",
-			jQuery: "jquery"	// for UploaderJS
-		})
-	]
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      hash: true,
+      template: './src/pdf-img-viewer-uploader/index.html',
+      filename: 'index.html'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'	// for UploaderJS
+    })
+  ]
 };
